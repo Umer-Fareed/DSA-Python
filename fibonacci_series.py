6bd4b1c4e30250675c427a94be1a07fdaf8c0cec
Umer-Fareed <muhammad.umer.5121124@gmail.com>
2025-09-01 10:39:13 +0500

    day 1 in fibonacci series algorithm

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,8 +1,9 @@
 #Find fibonacci sequence
+reputations= int(input('how much time to run? \n'))
 prev1= 0
 prev2= 1
 
-for fibo in range(18):
+for _  in range(reputations):
     newfibo= prev1 + prev2
     print(newfibo)
     prev1= prev2
